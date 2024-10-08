fb8e76a66a93c312896cfca4d68e7cf85e814ce7
yusuke <yske.biz@gmail.com>
2024-10-08 21:12:23 +0900

    add: form send

--- diary/views.py
+++ diary/views.py
@@ -1,8 +1,11 @@
+import logging
+
+from django.urls import reverse_lazy
 from django.views import generic
-
 from .forms import InquiryForm
+from django.contrib import messages
 
-# Create your views here.
+logger = logging.getLogger(__name__)
 
 
 class IndexView(generic.TemplateView):
@@ -12,3 +15,10 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy("diary:inquiry")
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, "メッセージを送信しました。")
+        logger.info("inquiry sent by {}".format(form.cleaned_data["name"]))
+        return super().form_valid(form)
